161a9dedcd565d55c2b694dcc336b530e206fa28
Jasper <jaspertaylor15@protonmail.com>
2025-10-09 13:14:15 -1000

    fix: auto-fix attempt 1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -20,7 +20,7 @@ installClientErrorReporter();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <ErrorBoundary>
         <App />
       </ErrorBoundary>
